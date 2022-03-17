c867aef6ccafae001c751186d260ee03818a24a0
Sebastiaan Lindenau <Sebastiaan.Lindenau@Honeywell.com>
2022-03-17 09:47:34 +0100

    Harden parsing of track duration

--- src/main/java/nl/slindenau/openmusicscrobbler/service/TrackDurationService.java
+++ src/main/java/nl/slindenau/openmusicscrobbler/service/TrackDurationService.java
@@ -6,7 +6,7 @@ import java.util.EmptyStackException;
 import java.util.Stack;
 
 /**
- * Parses hour:minute:second notation to a total length in seconds
+ * Parses <code>hour:minute:second</code> or <code>minute:second</code> notation to a total length in seconds
  *
  * @author slindenau
  * https://github.com/s-lindenau
@@ -14,18 +14,17 @@ import java.util.Stack;
  */
 public class TrackDurationService {
 
-    public static final String DEFAULT_DURATION_SEPARATOR = ":";
-    public static final int SECONDS_MULTIPLIER_TO_SECONDS = 1;
-    public static final int MINUTE_MULTIPLIER_TO_SECONDS = 60 * SECONDS_MULTIPLIER_TO_SECONDS;
-    public static final int HOUR_MULTIPLIER_TO_SECONDS = 60 * MINUTE_MULTIPLIER_TO_SECONDS;
+    private static final String DEFAULT_DURATION_SEPARATOR = ":";
+    private static final int SECONDS_MULTIPLIER_TO_SECONDS = 1;
+    private static final int MINUTE_MULTIPLIER_TO_SECONDS = 60 * SECONDS_MULTIPLIER_TO_SECONDS;
+    private static final int HOUR_MULTIPLIER_TO_SECONDS = 60 * MINUTE_MULTIPLIER_TO_SECONDS;
 
-    private Stack<Integer> multiplicationFactors = new Stack<>();
-
-    private void createMultiplicationFactorsStack() {
-        multiplicationFactors = new Stack<>();
+    private Stack<Integer> createMultiplicationFactorsStack() {
+        Stack<Integer> multiplicationFactors = new Stack<>();
         multiplicationFactors.push(HOUR_MULTIPLIER_TO_SECONDS);
         multiplicationFactors.push(MINUTE_MULTIPLIER_TO_SECONDS);
         multiplicationFactors.push(SECONDS_MULTIPLIER_TO_SECONDS);
+        return multiplicationFactors;
     }
 
     public int parseTrackLength(String trackDuration) {
@@ -40,11 +39,12 @@ public class TrackDurationService {
     private int toSeconds(String[] trackDurationParts) {
         int partsCount = trackDurationParts.length;
         int timeInSeconds = 0;
+        Stack<Integer> multiplicationFactors = createMultiplicationFactorsStack();
         // reverse order; so we start with seconds, which is the top item in our Stack
         for (int i = partsCount - 1; i >= 0; i--) {
             String currentPart = trackDurationParts[i];
             Integer currentPartLength = parseInteger(currentPart);
-            Integer multiplicationFactor = getNextMultiplicationFactor();
+            Integer multiplicationFactor = getNextMultiplicationFactor(multiplicationFactors);
             timeInSeconds += currentPartLength * multiplicationFactor;
         }
         return timeInSeconds;
@@ -58,7 +58,7 @@ public class TrackDurationService {
         }
     }
 
-    private Integer getNextMultiplicationFactor() {
+    private Integer getNextMultiplicationFactor(Stack<Integer> multiplicationFactors) {
         try {
             return multiplicationFactors.pop();
         } catch (EmptyStackException ex) {
