1cfbb7f01792ade53e6b29eff6720868712d63ab
slindenau <slindenau@home.nl>
2022-05-26 13:28:33 +0200

    Add test class to get a Discogs release

---
+++ src/test/java/nl/slindenau/openmusicscrobbler/test/DiscogsClientReleaseDownloader.java
@@ -0,0 +1,35 @@
+package nl.slindenau.openmusicscrobbler.test;
+
+import nl.slindenau.openmusicscrobbler.discogs.client.DiscogsClientFactory;
+import nl.slindenau.openmusicscrobbler.discogs.client.DiscogsClientWrapper;
+
+import java.util.Arrays;
+
+/**
+ * Test utility to print the JSON data of a given Discogs release by id.
+ *
+ * @author slindenau
+ * https://github.com/s-lindenau
+ * Licence: GPLv3
+ */
+public class DiscogsClientReleaseDownloader {
+    public static void main(String[] commandLineArguments) {
+        String releaseId = getReleaseId(commandLineArguments);
+        // enable debug to log the JSON response
+        System.setProperty("oms.debug", "true");
+        DiscogsClientWrapper client = new DiscogsClientFactory().getClient();
+        client.getRelease(releaseId);
+    }
+
+    private static String getReleaseId(String[] commandLineArguments) {
+        try {
+            String releaseIdArgument = commandLineArguments[0];
+            Integer.parseInt(releaseIdArgument);
+            return releaseIdArgument;
+        } catch (Exception ex) {
+            String message = "Invalid release id on command line argument index 0: ";
+            System.out.println(message + Arrays.asList(commandLineArguments == null ? new String[]{} : commandLineArguments));
+            throw ex;
+        }
+    }
+}
