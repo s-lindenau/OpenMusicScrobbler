d2839809c88a1cffdf62d413c228190fef302ecf
Sebastiaan Lindenau <Sebastiaan.Lindenau@Honeywell.com>
2023-11-16 12:09:45 +0100

    Update scrobble.js with JSDoc and declare variables

--- src/main/resources/nl/slindenau/openmusicscrobbler/web/assets/scrobble.js
+++ src/main/resources/nl/slindenau/openmusicscrobbler/web/assets/scrobble.js
@@ -1,20 +1,29 @@
+/**
+ * Scrobble the selected release and update the document with feedback
+ * @param id the discogs ID of the release to scrobble
+ * @returns {Promise<void>} no data is returned when this Promise completes
+ */
 async function scrobble(id) {
-    scrobbleButtonElementId = "scrobbleButton";
-    scrobbleResultElementId = "scrobbleResultFeedback";
+    const scrobbleButtonElementId = "scrobbleButton";
+    const scrobbleResultElementId = "scrobbleResultFeedback";
 
     document.getElementById(scrobbleButtonElementId).disabled = true;
     document.getElementById(scrobbleResultElementId).innerHTML = "Submitting scrobble...";
 
-    result = await postScrobble(id);
+    let result = await postScrobble(id);
     document.getElementById(scrobbleResultElementId).innerHTML = result.message;
 
     document.getElementById(scrobbleButtonElementId).disabled = false;
 }
 
+/**
+ * Call the API method to scrobble the given release
+ * @param id the discogs ID of the release to scrobble
+ * @returns {Promise<any>} the result of the POST scrobble call as JSON data
+ */
 async function postScrobble(id) {
     const url = "/app/collection/scrobble?id=" + id;
     const parameters = {method: "POST"};
-    response = await fetch(url, parameters);
-    data = await response.json();
-    return data;
+    let response = await fetch(url, parameters);
+    return await response.json();
 }
