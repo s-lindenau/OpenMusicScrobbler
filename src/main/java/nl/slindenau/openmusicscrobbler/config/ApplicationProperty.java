39176b817d5194b940a657c2b41cd3161bb7bd8b
Sebastiaan Lindenau <Sebastiaan.Lindenau@Honeywell.com>
2024-05-16 11:40:38 +0200

    ApplicationProperty enum readability

--- src/main/java/nl/slindenau/openmusicscrobbler/config/ApplicationProperty.java
+++ src/main/java/nl/slindenau/openmusicscrobbler/config/ApplicationProperty.java
@@ -9,17 +9,58 @@ import java.util.Optional;
  */
 public enum ApplicationProperty {
 
-    DISCOGS_USERNAME("discogs.username", "Discogs username"),
-    DISCOGS_TOKEN("discogs.personal.token", "Discogs personal access token (Optional), see https://www.discogs.com/settings/developers", true),
-    LAST_FM_USERNAME("lastfm.username", "Last.fm username"),
-    LAST_FM_PASSWORD("lastfm.password", "Last.fm password", true),
-    LAST_FM_API_KEY("lastfm.api.key", "Last.fm API key, see https://www.last.fm/api/authentication"),
-    LAST_FM_API_SECRET("lastfm.api.secret", "Last.fm API secret", true),
-    DISCOGS_CONNECTION_TIMEOUT("discogs.connection.timeout", "Timeout for Discogs API connection setup (Java Duration format)", "PT10S"),
-    DISCOGS_READ_TIMEOUT("discogs.read.timeout", "Timeout for Discogs API call results (Java Duration format)", "PT60S"),
-    DISCOGS_TRACK_LENGTH("discogs.track.length.default", "Default track duration to substitute a missing duration on Discogs (Java Duration format)", "PT1M"),
-    DEBUG("oms.debug", "Enable debug logging [true or false]. Scrobbles are not sent to Last.FM when enabled.", Boolean.FALSE.toString()),
-    LOG_LEVEL("oms.log.level", "Determines what logging information is written to the log file. Possible values are Logback levels (logging library)", "info"),
+    DISCOGS_USERNAME(
+            "discogs.username",
+            "Discogs username"
+    ),
+    DISCOGS_TOKEN(
+            "discogs.personal.token",
+            "Discogs personal access token (Optional), see https://www.discogs.com/settings/developers",
+            true
+    ),
+    LAST_FM_USERNAME(
+            "lastfm.username",
+            "Last.fm username"
+    ),
+    LAST_FM_PASSWORD(
+            "lastfm.password",
+            "Last.fm password",
+            true
+    ),
+    LAST_FM_API_KEY(
+            "lastfm.api.key",
+            "Last.fm API key, see https://www.last.fm/api/authentication"
+    ),
+    LAST_FM_API_SECRET(
+            "lastfm.api.secret",
+            "Last.fm API secret",
+            true
+    ),
+    DISCOGS_CONNECTION_TIMEOUT(
+            "discogs.connection.timeout",
+            "Timeout for Discogs API connection setup (Java Duration format)",
+            "PT10S"
+    ),
+    DISCOGS_READ_TIMEOUT(
+            "discogs.read.timeout",
+            "Timeout for Discogs API call results (Java Duration format)",
+            "PT60S"
+    ),
+    DISCOGS_TRACK_LENGTH(
+            "discogs.track.length.default",
+            "Default track duration to substitute a missing duration on Discogs (Java Duration format)",
+            "PT1M"
+    ),
+    DEBUG(
+            "oms.debug",
+            "Enable debug logging [true or false]. Scrobbles are not sent to Last.FM when enabled.",
+            Boolean.FALSE.toString()
+    ),
+    LOG_LEVEL(
+            "oms.log.level",
+            "Determines what logging information is written to the log file. Possible values are Logback levels (logging library)",
+            "info"
+    ),
 
     ;
 
