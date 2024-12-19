c29b4ad4730161162154bf4359bc1c2c30011441
Sebastiaan Lindenau <Sebastiaan.Lindenau@Honeywell.com>
2024-12-19 14:49:37 +0100

    Remove command line prototype

--- src/main/java/nl/slindenau/openmusicscrobbler/Application.java
+++ src/main/java/nl/slindenau/openmusicscrobbler/Application.java
@@ -1,7 +1,5 @@
 package nl.slindenau.openmusicscrobbler;
 
-import nl.slindenau.openmusicscrobbler.cli.ConfigurationWizardClient;
-import nl.slindenau.openmusicscrobbler.cli.ConsoleClient;
 import nl.slindenau.openmusicscrobbler.exception.LoggingUncaughtExceptionHandler;
 import nl.slindenau.openmusicscrobbler.web.WebApplication;
 import org.slf4j.bridge.SLF4JBridgeHandler;
@@ -13,20 +11,9 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
  */
 public class Application {
 
-    private static final String CONSOLE_APPLICATION = "console";
-    private static final String CONFIG_APPLICATION = "config";
-    private static final String SERVER_APPLICATION = "server";
-    private static final String DEFAULT_APPLICATION_TO_RUN = CONSOLE_APPLICATION;
-
     public static void main(String[] commandLineArguments) {
         configureApplication();
-        String applicationToRun = getApplicationToRun(commandLineArguments);
-        switch (applicationToRun) {
-            case CONSOLE_APPLICATION -> new ConsoleClient().run();
-            case CONFIG_APPLICATION -> new ConfigurationWizardClient().run();
-            case SERVER_APPLICATION -> new WebApplication().run(commandLineArguments);
-            default -> throw new IllegalArgumentException("Unknown command: " + applicationToRun);
-        }
+        new WebApplication().run(commandLineArguments);
     }
 
     private static void configureApplication() {
@@ -41,12 +28,5 @@ public class Application {
     private static void configureLogging() {
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
-    }
-
-    private static String getApplicationToRun(String[] commandLineArguments) {
-        if (commandLineArguments == null || commandLineArguments.length == 0) {
-            return DEFAULT_APPLICATION_TO_RUN;
-        }
-        return commandLineArguments[0];
     }
 }
